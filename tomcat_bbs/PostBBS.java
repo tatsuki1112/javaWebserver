145035361534730ec2545d5953a14a68a9633b33
tatsuki1112@gmail.com <tatsuki1112@gmail.com>
2021-01-06 22:09:17 +0900

    bbs tomcat

---
+++ tomcat_bbs/PostBBS.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import javax.servlet.http.*;
+
+public class PostBBS extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws UnsupportedEncodingException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        Message newMessage = new Message(request.getParameter("title"), request.getParameter("handle"), request.getParameter("message"));
+        Message.messageList.add(0, newMessage);
+        response.sendRedirect("/testbbs/ShowBBS");
+    }
+}
